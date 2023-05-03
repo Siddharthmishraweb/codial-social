2ced0efd790e71516991eab14e343cd694a339c2
Siddharth Mishra <smishra@anuktaitsolutions.com>
2023-05-03 19:58:15 +0530

    set the view engine

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router();
+const homeController = require('../controllers/home_controller')
+
+router.get('/', homeController.home);
+router.use('/users', require('./users'))
+
+// for any further routes , access from here
+// router.use('/routerName', require('./routerFile'));
+
+console.log('routes indx.js is loaded');
+module.exports = router;
