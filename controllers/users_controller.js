80f351e35e744c51891722925fd499e7ab326e4a
Siddharth Mishra <smishra@anuktaitsolutions.com>
2023-05-05 19:14:14 +0530

    Added header and footer i.e Partials in ejjs

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,6 @@
 module.exports.profile = function(req, res){
-   return res.end('<h1> You are in users controller </h1>');
+   // return res.end('<h1> You are in users controller </h1>');
+   return res.render('user_profile', {
+      title: "Codial Profile Page"
+   });
 }
