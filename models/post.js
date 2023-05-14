5502cce280f5b710fc76433c0ec62b852e6f602e
smishra-facilgo <smishra@anuktaitsolutions.com>
2023-05-14 17:42:45 +0530

    added view of posts

---
+++ models/post.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+   content:{
+      type: String,
+      required: true
+   },
+   user:{
+      type:mongoose.Schema.Types.ObjectId,
+      // refered to which Schema
+      ref:'User'
+   }
+},{
+    timestamps: true
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
