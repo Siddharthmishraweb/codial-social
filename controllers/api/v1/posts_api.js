4f5733696a5d59c6814d47c16b58fd393cb4e718
Siddharthmishraweb <89569473+smishra-facilgo@users.noreply.github.com>
2023-05-25 05:59:38 +0530

    Created Bearer token from jwt, created users api'

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,4 +1,5 @@
 const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
 module.exports.index = async function(req, res){
    let posts = await Post.find({})
    .sort('-createdAt')
@@ -16,4 +17,20 @@ module.exports.index = async function(req, res){
       message: "Lists of post",
       posts: posts
    })
+}
+ 
+module.exports.destroy = async function(req, res){
+   try{
+      let post = await Post.findById(req.params.id);
+      await post.deleteOne();
+      await Comment.deleteMany({post:req.params.id});
+      return res.json(200, {
+         message: "Post and associated comments are deleted successfully!"
+      });
+   }catch(err){
+      console.log("***** -ERROR- *****", err)
+      return res.json(500,{
+         message: 'Internal Server Error'
+      });
+   }
 }
