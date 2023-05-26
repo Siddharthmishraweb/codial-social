050e112320ef4a0e1d5cb833b6862dde52f7c5a4
Siddharthmishraweb <89569473+smishra-facilgo@users.noreply.github.com>
2023-05-26 20:42:37 +0530

    mail sending on posting comments working fine

--- mailers/comments_mailer.js
+++ mailers/comments_mailer.js
@@ -2,12 +2,15 @@ const nodeMailer = require('../config/nodemailer');
 
 // this is another way of exporting a method
 exports.newComment = (comment) => {
+   let htmlString = nodeMailer.renderTemplate({comment: comment}, '/comments/new_comment.ejs');
+
+
    console.log('Inside newComment mailer', comment);
    nodeMailer.transporter.sendMail({
       from: 'siddharth23mishra@gmail.com',
       to: comment.user.email,
       subject: "New Comment Published",
-      html: '<h1>Yup! your comment is now published</h1>'
+      html: htmlString
    },(err, info) => {
        if(err){console.log('Error in sending mail', err); return;}
        console.log('Message sent! ', info);
