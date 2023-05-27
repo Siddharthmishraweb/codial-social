47e78146bb772d49c7e4e2fc5c3d88dd5d4d7f48
Siddharthmishraweb <89569473+smishra-facilgo@users.noreply.github.com>
2023-05-27 16:03:46 +0530

    created action and routes for Like

---
+++ controllers/likes_controller.js
@@ -0,0 +1,54 @@
+const Like = require('../models/like');
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+
+// when someone clicks first time post like count increases when he likes second times likes count decreaces for that toggle is implemented
+module.exports.toogleLike = async function(req, res){
+   try{
+      // likes/toggle/?id=abcdef&type=Post
+      let likeable;
+      let deleted = false;
+      if(req.query.type == 'Post'){
+         likeable = await Post.findById(req.query.id).populate('likes');
+      }else{
+         likeable = await Comment.findById(req.query.id).populate('likes');
+      }
+
+      //check if a like already exist
+      let existingLike = await Like.findOne({
+         likeable: req.query.id,
+         onModel: req.query.type
+      })
+
+      // if a like already exist then delete it
+      if(existingLike){
+        likeable.likes.pull(existingLike._id);
+        likeable.save();
+        existingLike.remove();
+        deleted: true;
+      }else{
+         // make a  new like
+         let newLike = await Like.create({
+            user: req.user._id,
+            likeable: req.query.id,
+            onModel: req.query.type
+         });
+         likeable.likes.push(newLike._id);
+         likeable.save();
+      }
+      return res.json(200, {
+         message: "Request Successfull",
+         data:{
+            deleted: deleted
+         }
+      })
+
+   }catch(err){
+      console.log(err);
+      return res.json(500, {
+         message: 'Internal Server Error'
+      })
+
+   }
+
+}
